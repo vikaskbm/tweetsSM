b6dc095aff513a7560e01e0aeef003d4ba6427f3
Vikas Bishnoi <vikaskbm@gmail.com>
2020-08-11 13:26:02 +0530

    imported getoldtweets3 and implemented function to get tweets of inputted usernames

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -4,15 +4,41 @@ import string
 import json
 import matplotlib.pyplot as plt
 from collections import Counter
+import GetOldTweets3 as got
 
-text = 'I love my country!!'
+
+# getting tweets from twitter user using getoldtweets3
+def get_tweets(username):
+    import GetOldTweets3 as got
+    tweetCriteria = got.manager.TweetCriteria().setUsername(username) \
+        .setSince("2020-01-01") \
+        .setUntil("2020-04-01") \
+        .setMaxTweets(10)
+    # Creation of list that contains all tweets
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+    # Creating list of chosen tweet data
+    tweets = [[tweet.text] for tweet in tweets]
+    return tweets
+
+
+username = input("Enter twitter username(multiple userneame separated by space)")
+
+text = ""
+text_tweets = get_tweets(username)
+length = len(text_tweets)
+print(text_tweets[0])
+
+for i in range(0, length):
+    text = text_tweets[i][0] + " " + text
+
+print(text)
+
 # lowercasing and cleaning the input text
 text = text.lower()
 cleaned_text = text.translate(str.maketrans('', '', string.punctuation))
 
 # tokenizing the text using word_tokenize function
 text_tokenize = word_tokenize(text)
-print(text_tokenize)
 
 
 # Removing stop words from our text
@@ -35,7 +61,6 @@ for word in final_text:
 
 # count occurance of all the emotions in the input
 emotion_count = Counter(emotion_list)
-print(emotion_count)
 
 
 # plotting data on a graph
